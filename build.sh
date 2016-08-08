c96efa647e1e07ca113bfcce8bb67899ff53aeb8
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2016-08-08 10:43:46 +0200

    fix build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ if [ ! -d build ] ; then
     cd build
     cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr -D CPACK_GENERATOR=DEB -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ..
     make -j 24
+    export LD_LIBRARY_PATH=${PWD}/lib
     cpack
 else
     echo "Build seems done. Clean up and try again."
